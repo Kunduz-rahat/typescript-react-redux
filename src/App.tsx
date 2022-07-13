be9e41a2ee1dc08820ad93a79bcb4c946ba31556
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-07-13 12:12:57 +0600

    add Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import Card, {CardVariant} from './components/Card';
 
 
 function App() {
   return (
     <div >
-    
-        <p>
-         app
-        </p>
-   
+      <Card variant={CardVariant.primary} height='200px' width='200px'>
+        <button>Кнопка</button>
+        <div>dfssfsf</div>
+      </Card>
     </div>
   );
 }
